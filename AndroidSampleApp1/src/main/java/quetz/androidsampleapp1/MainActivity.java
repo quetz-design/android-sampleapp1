52e2d6bce45515608587da9c97e76d88f34ff596
Kouta Yoshimura <quetz.design@gmail.com>
2014-01-02 15:27:23 +0900

    Respond to the Send Button

--- AndroidSampleApp1/src/main/java/quetz/androidsampleapp1/MainActivity.java
+++ AndroidSampleApp1/src/main/java/quetz/androidsampleapp1/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -32,6 +33,10 @@ public class MainActivity extends ActionBarActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view){
+        //Do something
     }
 
 }
